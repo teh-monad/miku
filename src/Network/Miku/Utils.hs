485c4069c6d20a7189eb29556a60896a8a7f7c39
Jinjing Wang <nfjinjing@gmail.com>
2016-03-16 18:24:35 +0800

    clean up

--- src/Network/Miku/Utils.hs
+++ src/Network/Miku/Utils.hs
@@ -2,18 +2,18 @@
 
 module Network.Miku.Utils where
 
-import Control.Lens ((#), (&))
-import Control.Monad.State
-import Data.Bifunctor (first)
-import Data.ByteString.Char8 (ByteString)
-import Data.Monoid ((<>))
-import Prelude hiding ((-))
+import           Control.Lens          (( # ), (&))
+import           Control.Monad.State
+import           Data.Bifunctor        (first)
+import           Data.ByteString.Char8 (ByteString)
 import qualified Data.ByteString.Char8 as B
+import           Data.CaseInsensitive  (CI)
+import qualified Data.CaseInsensitive  as CI
+import           Data.Monoid           ((<>))
+import           Data.String
+import qualified Network.HTTP.Types    as H
 import           Network.Wai
-import qualified Network.HTTP.Types           as H
-import Data.String
-import           Data.CaseInsensitive  ( CI )
-import qualified Data.CaseInsensitive as CI
+import           Prelude               hiding ((-))
 
 infixr 0 -
 {-# INLINE (-) #-}
