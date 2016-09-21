e7ba0cbe57c0ba22289411eea0c90364b5e51bcb
Jinjing Wang <nfjinjing@gmail.com>
2016-09-21 15:30:23 +0800

    update hello example

--- test/HelloWorld.hs
+++ test/HelloWorld.hs
@@ -6,9 +6,16 @@ import           Network.Miku
 
 import Network.HTTP.Pony.Serve (run)
 import Network.HTTP.Pony.Serve.Wai (fromWAI)
+import Network.HTTP.Pony.Transformer.HTTP (http)
+import Network.HTTP.Pony.Transformer.CaseInsensitive (caseInsensitive)
 import Pipes.Safe (runSafeT)
 
 main :: IO ()
 main =
-  runSafeT . (run "localhost" "8080") . fromWAI . miku $
+  runSafeT
+    . (run "localhost" "8080")
+    . http
+    . caseInsensitive
+    . fromWAI
+    . miku $
     get "/" (text "miku power")
